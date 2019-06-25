af86fa2721f7bb607a5d3a4b733a26241104926c
Isabel <william@williamlomas.me>
2019-06-24 23:04:58 -0700

    minimal

--- bot.py
+++ bot.py
@@ -8,5 +8,13 @@ For some reason vscode marks "HACK" as another color than the comment color in p
 
 from datetime import datetime
 import discord
+from discord.ext import commands
 import json
 import requests
+
+config = json.load(open('config.json'))
+
+bot = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or('sdow-'), case_insensitive=True, activity=discord.Game(name='Six Degrees of Wikipedia!'))
+bot.remove_command('help')
+
+bot.run(config['token'])
