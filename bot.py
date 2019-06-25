e4805696952d80a89d2bae31b00f093be1c14d61
Isabel <william@williamlomas.me>
2019-06-25 04:49:39 -0700

    hackweek server probably wont stick around

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ b.remove_command('help')
 async def i(o):
     e = discord.Embed(color=0x68ceff, description='Made by Isabel#0002', title='About SDoW')
     e.add_field(name='About', value='Six Degrees of Wikipedia (The Discord Bot) is made for Discord Hack Week in 2019.\nThe bot is based off the [Six Degrees of Wikipedia](https://sixdegreesofwikipedia.com) game.', inline=False)
-    e.add_field(name='Links', value='[Discord Hack Week](https://discord.gg/hackweek)\n[discord.py](https://github.com/Rapptz/discord.py)\n[GitHub](https://github.com/Mippy/SDOW-Bot)\n[Six Degrees of Wikipedia](https://sixdegreesofwikipedia.com)\n[SDoW GitHub](https://github.com/jwngr/sdow)', inline=False)
+    e.add_field(name='Links', value='[discord.py](https://github.com/Rapptz/discord.py)\n[GitHub](https://github.com/Mippy/SDOW-Bot)\n[Six Degrees of Wikipedia](https://sixdegreesofwikipedia.com)\n[SDoW GitHub](https://github.com/jwngr/sdow)', inline=False)
     e.set_thumbnail(url='https://raw.githubusercontent.com/wikimedia/portals/master/dev/wikipedia.org/assets/img/Wikipedia-logo-v2.png')
     e.timestamp = datetime.utcnow()
     e.set_footer(text=f'Requested by {o.author} ({o.author.id})')
