99295c8e00b313ddfae938100a821bd707f5f138
Isabel <william@williamlomas.me>
2019-06-25 03:44:46 -0700

    remove unnecessary stuff

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@
 
 """
 Made by Isabel Lomas for DISCORD HACK WEEK 2019!
-
-For some reason vscode marks "HACK" as another color than the comment color in python's multiline comments.
 """
 
 from datetime import datetime
