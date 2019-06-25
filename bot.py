1dc9f0f5b87777f896ed60a9534bd2e249395202
Isabel <william@williamlomas.me>
2019-06-24 23:29:13 -0700

    info command

--- bot.py
+++ bot.py
@@ -12,9 +12,19 @@ from discord.ext import commands
 import json
 import requests
 
-config = json.load(open('config.json'))
+c = json.load(open('config.json'))
 
-bot = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or('sdow-'), case_insensitive=True, activity=discord.Game(name='Six Degrees of Wikipedia!'))
-bot.remove_command('help')
+b = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or('sdow-'), case_insensitive=True, activity=discord.Game(name='Six Degrees of Wikipedia!'))
+b.remove_command('help')
 
-bot.run(config['token'])
+@b.command(name='info')
+async def i(o):
+    e = discord.Embed(color=0x68ceff, description="Made by Isabel#0002", title='About SDoW')
+    e.add_field(name='About', value='Six Degrees of Wikipedia (The Discord Bot) is made for Discord Hack Week in 2019.\nThe bot is based off the [Six Degrees of Wikipedia](https://sixdegreesofwikipedia.com) game.', inline=False)
+    e.add_field(name='Links', value='[Discord Hack Week](https://discord.gg/hackweek)\n[discord.py](https://github.com/Rapptz/discord.py)\n[GitHub](https://github.com/Mippy/SDOW-Bot)\n[Six Degrees of Wikipedia](https://sixdegreesofwikipedia.com)\n[SDoW GitHub](https://github.com/jwngr/sdow)', inline=False)
+    e.set_thumbnail(url='https://raw.githubusercontent.com/wikimedia/portals/master/dev/wikipedia.org/assets/img/Wikipedia-logo-v2.png')
+    e.timestamp = datetime.utcnow()
+    e.set_footer(text=f'Requested by {o.author} ({o.author.id})')
+    await o.send(embed=e)
+
+b.run(c['token'])
