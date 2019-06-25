a83e2c122973cdb8a4e224047f80eeea4bc52b99
Isabel <william@williamlomas.me>
2019-06-24 22:55:26 -0700

    IMPORT IMPORT IMPORT IMPORT

---
+++ bot.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3.6
+
+"""
+Made by Isabel Lomas for DISCORD HACK WEEK 2019!
+
+For some reason vscode marks "HACK" as another color than the comment color in python's multiline comments.
+"""
+
+from datetime import datetime
+import discord
+import json
+import requests
