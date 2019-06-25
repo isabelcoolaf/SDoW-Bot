49797606a534f28e9af4107d0aff0dc34108f793
Isabel <william@williamlomas.me>
2019-06-25 03:44:55 -0700

    single quote

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ b.remove_command('help')
 
 @b.command(name='info')
 async def i(o):
-    e = discord.Embed(color=0x68ceff, description="Made by Isabel#0002", title='About SDoW')
+    e = discord.Embed(color=0x68ceff, description='Made by Isabel#0002', title='About SDoW')
     e.add_field(name='About', value='Six Degrees of Wikipedia (The Discord Bot) is made for Discord Hack Week in 2019.\nThe bot is based off the [Six Degrees of Wikipedia](https://sixdegreesofwikipedia.com) game.', inline=False)
     e.add_field(name='Links', value='[Discord Hack Week](https://discord.gg/hackweek)\n[discord.py](https://github.com/Rapptz/discord.py)\n[GitHub](https://github.com/Mippy/SDOW-Bot)\n[Six Degrees of Wikipedia](https://sixdegreesofwikipedia.com)\n[SDoW GitHub](https://github.com/jwngr/sdow)', inline=False)
     e.set_thumbnail(url='https://raw.githubusercontent.com/wikimedia/portals/master/dev/wikipedia.org/assets/img/Wikipedia-logo-v2.png')
